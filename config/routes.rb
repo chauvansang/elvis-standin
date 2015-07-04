82d08a62ab388a0c7daf6b145feebce49004a319
Mai Anh Vu <me@maianhvu.com>
2015-07-04 18:00:04 +0800

    dashboard redirection and setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  get 'dashboard/index'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations'
+  }
   devise_scope :user do
-    get 'sign-in', to: 'devise/sessions#new'
-    get 'register', to: 'devise/registrations#new'
+    get 'sign-in', to: 'users/sessions#new'
+    get 'sign-out', to: 'users/sessions#destroy'
+    get 'register', to: 'users/registrations#new'
   end
+
+
   root 'pages#home'
 
   get 'pages/home'
